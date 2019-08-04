3930cdc30f55cf32ad5e590439c31e65a0a02e1c
Nicole Dow <nikki.dow71@gmail.com>
2019-08-04 02:16:16 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,24 @@ class Scraper
     linkedin =
     github = 
     blog = 
-    quote = 
+    quote = page.css(".profile-quote").text
+    bio = page.css(".details-container .bio-block.details-block p").text
+    
+    page.css(".social-icon-container a").each do |all_social_media|
+      all_social_media.each do |social_media|
+        if social_media[1].include?("twitter")
+          twitter = social_media[1]
+        elsif social_media[1].include?("linkedin")
+          linkedin = social_media[1]
+        elsif social_media[1].include?("github")
+          github = social_media[1]
+        end
+      binding.pry
+      end
+    end
+    
+    
+    
     
     student_info = {twitter: twitter, linkedin: linkedin, github: github, blog: blog, quote: quote}
     student_info
