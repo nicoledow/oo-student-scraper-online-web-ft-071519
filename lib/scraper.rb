3d63bec7963af65201c13b73cb3082752f234efb
Nicole Dow <nikki.dow71@gmail.com>
2019-08-04 01:37:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,32 +2,33 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-@@students = []
 
   def self.scrape_index_page(index_url)
-    doc = open(index_url)
-    info = Nokogiri::HTML(doc)
-    #scrape page for css selector that points to students
-    #add students to @@students array as a hash
-    #hash should have the keys :name, :location, :profile_url
+    scraped_students = []
+    info = Nokogiri::HTML(open(index_url))
+ 
     info.css(".student-card").each do |student_card|
-      student_hash = {}
-      student_hash[:name] = student_card.css(".card-text-container h4.student-name").text
-      student_hash[:location] = student_card.css(".card-text-container p.student-location").text
-      student_hash[:profile_url] = "https://learn-co-curriculum.github.io/student-scraper-test-page/" + "#{student_card.css("a").attribute("href").value}"
-      #binding.pry
-      @@students << student_hash
-      #binding.pry
+      name = student_card.css(".card-text-container h4.student-name").text
+      location = student_card.css(".card-text-container p.student-location").text
+      url = student_card.css("a").attribute("href").value
+      scraped_students << {name: name, location: location, profile_url: url}
     end
-  end
-
-  def self.scrape_profile_page(profile_url)
-    
+    scraped_students
   end
   
-  def self.students
-    @@students
+  
+  def self.scrape_profile_page(profile_url)
+    page = Nokogiri::HTML(open(profile_url))
+    twitter =
+    linkedin =
+    github = 
+    blog = 
+    quote = 
+    
+    student_info = {twitter: twitter, linkedin: linkedin, github: github, blog: blog, quote: quote}
+    student_info
   end
+  
 
 end
 
