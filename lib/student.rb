85b457fcd327a74834a1532d6bbb55ec65376138
Nicole Dow <nikki.dow71@gmail.com>
2019-08-04 02:56:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    students_array.each do |s|
     binding.pry
+    end
   end
 
   def add_student_attributes(attributes_hash)
