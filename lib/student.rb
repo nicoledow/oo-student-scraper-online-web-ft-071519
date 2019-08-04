ec5036bc9e58d52185b44d78859a2cebe8654077
Nicole Dow <nikki.dow71@gmail.com>
2019-08-04 03:01:07 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,11 +13,16 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |s|
+      Student.new(s)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attribute, val|
+      self.send("#{attribute}=", val)
+    end
+    self
   end
 
   def self.all
